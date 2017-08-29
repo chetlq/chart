8e2e402f81bc454944a9362f3459e419ddb3ff83
chetlq <chetl@mail.ru>
2017-08-29 19:33:58 +0300

    f

--- server0.js
+++ server0.js
@@ -13,6 +13,7 @@ var arr = [
     //console.log(JSON.stringify(arr));
     axios.post('https://google-chart.herokuapp.com/rr',arr).then(function(res) {
       console.log("statusCode: ", res.status); // <======= Here's the status code
+      console.log(res.data);
       //console.log("headers: ", res.headers);
     // console.log(response.headers);
     // console.log(response.config);
